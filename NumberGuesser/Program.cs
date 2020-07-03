a90445b06e0e6df6b8126c89579729e7171f3af3
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-07-02 19:27:12 -0700

    Added Comments

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -15,11 +15,13 @@ namespace NumberGuesser
         {
                 Console.WriteLine("Think of a number, between 1 and 10, I will try and guess it!");
                 bool guessingGame = true;
+                //creates a new Random object.
                 Random randNum = new Random();
                 
 
                 while (guessingGame)
                 {
+                    //creates a new random number
                     int guessedNum = randNum.Next(1, 10);
                     Console.WriteLine("Is it " + guessedNum + "?");
                     string userInput = Console.ReadLine();
