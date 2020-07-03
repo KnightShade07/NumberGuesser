ac4261efbff302c1e258f0da5d0052ec5e171f23
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-07-02 19:22:54 -0700

    Added numGuess Method

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace NumberGuesser
 {
@@ -6,9 +7,43 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //Test commit
+            numGuess();
             
         }
+
+        static void numGuess()
+        {
+                Console.WriteLine("Think of a number, between 1 and 10, I will try and guess it!");
+                bool guessingGame = true;
+                Random randNum = new Random();
+                
+
+                while (guessingGame)
+                {
+                    int guessedNum = randNum.Next(1, 10);
+                    Console.WriteLine("Is it " + guessedNum + "?");
+                    string userInput = Console.ReadLine();
+                    
+                    if(userInput.Equals("y"))
+                    {
+                        Console.WriteLine("I win!");
+                        guessingGame = false;
+                    }
+                }
+
+                
+
+
+
+
+        }
+
+      
+
+
+
+
+
+        }
     }
-}
+
